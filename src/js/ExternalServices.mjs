c84794c729fa38adb51a4f8ac2978b52a226f5af
AaronGutierrez <aroncho2002@hotmail.com>
2026-06-18 17:10:48 -0500

    Test hardcoded key

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,6 +1,4 @@
-console.log(import.meta.env);
-const rawgKey = import.meta.env.VITE_RAWG_API_KEY;
-console.log("RAWG KEY:", rawgKey);
+const rawgKey = "76127e77bad34554891765612377d607";
 const rawgBaseURL = "https://api.rawg.io/api";
 const cheapSharkBaseURL = "https://www.cheapshark.com/api/1.0";
 
